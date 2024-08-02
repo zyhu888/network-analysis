1752b6c0893a71c71e1ab6a5acd02312e1a1fc1e
zyhu888 <zeyu.hu@uconn.edu>
2024-08-02 18:44:45 -0400

    update code
    Updated code for calculating efficiency. Combine all code(except function) into one rmd file

--- code/utility.R
+++ code/utility.R
@@ -36,7 +36,7 @@ calculate_assortativity_for_institution <- function(institution) {
   
   # Calculate assortativity
   institution_assortativity <- gen_assort(data = institution_data)
-  cat("The assortativity coefficient for", institution, "is:", institution_assortativity, "\n")
+  cat("The assortativity coefficient for", institution, "subnetwork is:", institution_assortativity, "\n")
 }
 
 ##########################################
@@ -67,5 +67,66 @@ calculate_assortativity_for_specialty <- function(specialty) {
   
   # Calculate assortativity
   specialty_assortativity <- gen_assort(data = specialty_data)
-  cat("The assortativity coefficient for", specialty, "is:", specialty_assortativity, "\n")
+  cat("The assortativity coefficient for", specialty, "subnetwork is:", specialty_assortativity, "\n")
+}
+
+##########################################
+###### Calculate global efficiency #######
+##########################################
+global_efficiency <- function(graph) {
+  n <- vcount(graph)
+  path_lengths <- distances(graph, weights = E(graph)$weight)
+  efficiency <- sum(1 / path_lengths[path_lengths != 0 & path_lengths != Inf])
+  efficiency <- efficiency / (n * (n - 1))
+  return(efficiency)
+}
+
+##########################################
+####### Filter nodes and calculate #######
+### efficiency for given institutions  ###
+##########################################
+calculate_efficiency_for_institution <- function(institution) {
+  # Filter nodes that belong to the specified institution
+  institution_nodes <- nodes[nodes$Institution == institution, ]
+  
+  # Extract the IDs of these filtered nodes
+  institution_node_ids <- institution_nodes$Id
+  
+  # Filter edges where both Source and Target belong to the filtered nodes
+  institution_edges <- edges[edges$Source %in% institution_node_ids & edges$Target %in% institution_node_ids, ]
+  
+  # Create graph object
+  institution_g <- graph_from_data_frame(d = institution_edges, directed = FALSE)
+  
+  # Add weights to graph edges
+  E(institution_g)$weight <- institution_edges$Weight
+  
+  # Calculate and return the global efficiency for the subnetwork
+  institution_efficiency <- global_efficiency(institution_g)
+  cat("The global efficiency for the", institution, "subnetwork is:", institution_efficiency, "\n")
+}
+
+##########################################
+####### Filter nodes and calculate #######
+###  efficiency for given specialties  ###
+##########################################
+calculate_efficiency_for_specialty <- function(specialty) {
+  # Filter nodes that belong to the specified specialty
+  specialty_nodes <- nodes[nodes$Specialty == specialty, ]
+  
+  # Extract the IDs of these filtered nodes
+  specialty_node_ids <- specialty_nodes$Id
+  
+  # Filter edges where both Source and Target belong to the filtered nodes
+  specialty_edges <- edges[edges$Source %in% specialty_node_ids & edges$Target %in% specialty_node_ids, ]
+  
+  # Create graph object
+  specialty_g <- graph_from_data_frame(d = specialty_edges, directed = FALSE)
+  
+  # Add weights to graph edges
+  E(specialty_g)$weight <- specialty_edges$Weight
+  
+  # Calculate and return the global efficiency for the subnetwork
+  specialty_efficiency <- global_efficiency(specialty_g)
+  cat("The global efficiency for the", specialty, "subnetwork is:", specialty_efficiency, "\n")
 }
